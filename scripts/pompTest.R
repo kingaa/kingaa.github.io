c84c4cfee466caca0bf8c723dc367b317f0be160
Aaron A. King <kingaa@umich.edu>
2016-04-28 09:19:01 -0400

    install dependencies; make messages more informative

--- scripts/pompTest.R
+++ scripts/pompTest.R
@@ -4,10 +4,25 @@
 lib <- Sys.getenv("R_LIBS_USER")
 dir.create(lib,recursive=TRUE,showWarnings=FALSE)
 
+cat("Checking whether dependencies are installed....\n")
+## install dependencies if necessary
+deps <- setdiff(
+    c("digest","mvtnorm","deSolve","coda","subplex","nloptr"),
+    rownames(installed.packages())
+    )
+if (length(deps) > 0) {
+    cat("Installing dependencies....\n")
+    install.packages(deps,lib=lib)
+}
+
+## install pomp and pompExamples
+cat("Installing",sQuote("pomp"),"and",sQuote("pompExamples"),"....\n")
 install.packages(c("pomp","pompExamples"),
                  repos="http://kingaa.github.io",
                  lib=lib)
 
+## test pomp
+cat("Testing",sQuote("pomp"),"....\n")
 library(pomp,lib.loc=lib)
 
 gomp2 <- pomp(
@@ -39,4 +54,4 @@ plot(gomp2)
 p <- pfilter(gomp2,Np=1000)
 plot(p)
 
-cat("'pompTest' successful!\n")
+cat("Success!\n")

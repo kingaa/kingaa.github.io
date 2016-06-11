c3032e799c2a6c6521de4d8218f9ba632c832e75
Aaron A. King <kingaa@umich.edu>
2016-06-11 10:09:22 -0400

    test fortran as well as C

--- scripts/hello.R
+++ scripts/hello.R
@@ -1,11 +1,33 @@
-cat("#include <R.h>
+try({
+    cat("#include <R.h>
      void hello (void) {
-     Rprintf(\"hello world!\\n\");
-     }",
-    file="hello.c")
+     Rprintf(\"\\\n\\n\\nHello world! from native C\\n\\n\\n\");
+     }",file="helloC.c")
+    system2(command=R.home("bin/R"),
+            args=c("CMD","SHLIB","helloC.c"))
+    dyn.load(paste0("helloC",.Platform$dynlib.ext))
+    invisible(.C("hello",PACKAGE="helloC"))
+    dyn.unload(paste0("helloC",.Platform$dynlib.ext))
+    file.remove(list.files(pattern="^helloC\\..*"))
+})
 
-system("R CMD SHLIB hello.c")
-
-dyn.load(paste0("hello",.Platform$dynlib.ext))
-
-.C("hello",PACKAGE="hello")
+try({
+    cat("
+      subroutine hello
+      external dblepr
+      call dblepr(\" \",-1,0,0)
+      call dblepr(\" \",-1,0,0)
+      call dblepr(\" \",-1,0,0)
+      call dblepr(\"hello world! from native FORTRAN\",-1,0,0)
+      call dblepr(\" \",-1,0,0)
+      call dblepr(\" \",-1,0,0)
+      call dblepr(\" \",-1,0,0)
+      end
+",file="helloF.f")
+    system2(command=R.home("bin/R"),
+            args=c("CMD","SHLIB","helloF.f"))
+    dyn.load(paste0("helloF",.Platform$dynlib.ext))
+    invisible(.Fortran("hello",PACKAGE="helloF"))
+    dyn.unload(paste0("helloF",.Platform$dynlib.ext))
+    file.remove(list.files(pattern="^helloF\\..*"))
+})

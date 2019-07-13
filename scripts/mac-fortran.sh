7a77f1e64303af1123246ec8f72ce2f227be29c1
Aaron A. King <kingaa@umich.edu>
2019-07-13 05:09:11 -0400

    update link

--- scripts/mac-fortran.sh
+++ scripts/mac-fortran.sh
@@ -3,7 +3,7 @@
 echo "downloading gfortran-4.8.2 and unpacking it"
 
 mkdir -p ~/gfortran
-curl -O http://r.research.att.com/libs/gfortran-4.8.2-darwin13.tar.bz2
+curl -OL https://mac.r-project.org/libs/gfortran-4.8.2-darwin13.tar.bz2
 tar -C ~/gfortran -jxvf gfortran-4.8.2-darwin13.tar.bz2
 
 echo "cleaning up: removing tarball"

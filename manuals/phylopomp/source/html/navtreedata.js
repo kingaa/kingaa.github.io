e95073a32ea1e1b376b5be521649e0e2fdd68cb9
Aaron A. King <kingaa@umich.edu>
2022-06-02 14:08:09 -0400

    phylopomp v 0.0.999.11

--- manuals/phylopomp/source/html/navtreedata.js
+++ manuals/phylopomp/source/html/navtreedata.js
@@ -1,38 +1,11 @@
 var NAVTREE =
 [
-  [ "phylopomp", "index.html", [
-    [ "Source code for Markov genealogy process simulators", "index.html", null ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ],
-        [ "Typedefs", "functions_type.html", null ],
-        [ "Related Functions", "functions_rela.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ],
-        [ "Typedefs", "globals_type.html", null ],
-        [ "Enumerations", "globals_enum.html", null ],
-        [ "Enumerator", "globals_eval.html", null ],
-        [ "Macros", "globals_defs.html", null ]
-      ] ]
-    ] ]
-  ] ]
+  [ "phylopomp", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"annotated.html",
-"classpopul__proc__t.html#ae10b9965fc1e537b75a934a3f51aaffe",
-"structsirwr__state__t.html#a0fd6eed842333aa9ccb9c877330f2573"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';

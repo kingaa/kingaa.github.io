32cc21da1d2fd3d2bdf08dff7a00388fe1e00d36
Aaron A. King <kingaa@umich.edu>
2023-07-21 11:19:58 -0400

    dofuture test script

--- scripts/dofuture_test.R
+++ scripts/dofuture_test.R
@@ -1,67 +1,64 @@
-library(foreach,quietly=TRUE)
-library(doFuture,quietly=TRUE)
-registerDoFuture()
+library(doFuture)
 
-njobs <- 1600
-nnode <- 220
-ncore <- 1
-chunk <- 1
+njobs <- 3000
+nnode <- 250
+nper <- njobs/nnode
 
-## set njobs, ncore, chunk from the command line
+## set njobs from the command line
 invisible(eval(parse(text=commandArgs(trailingOnly=TRUE))))
 
 cl <- makeClusterMPI(nnode,autostop=TRUE,verbose=TRUE)
 plan(cluster,workers=cl)
 
-cat("Starting computation of size",njobs,"using",
-  nnode,"nodes, with",
-  ncore,"cores/node,",
-  "and chunksize",chunk,"\n")
+cat("Starting computation of size",njobs,"using",nnode,"nodes",
+  "with",nper,"futures/worker","\n")
 
 tic <- Sys.time()
-res <- foreach (
+foreach (
   i = seq_len(njobs),
   .combine=rbind,
-  .inorder=FALSE
-) %dopar% {
+  .inorder=FALSE,
+  .options.future=list(seed=TRUE,schedule=nper)
+) %dofuture% {
   t1 <- Sys.time()
   h <- system("hostname",intern=TRUE)
   pid <- Sys.getpid()
-  x <- quantile(rnorm(n=1e8),prob=0.9)
+  x <- replicate(20,quantile(rnorm(n=1e7),prob=0.9))
+  m <- mean(x)
   t2 <- Sys.time()
-  data.frame(id=i,host=h,pid=pid,t1=t1,t2=t2,x=x)
-}
+  data.frame(id=i,host=h,pid=pid,t1=t1,t2=t2,x=m)
+} -> res
 toc <- Sys.time()
 
-suppressMessages(library(aakmisc,quietly=TRUE))
-library(magrittr)
-library(plyr)
-library(reshape2)
-library(ggplot2)
-library(grid)
-library(digest,quietly=TRUE)
+nwork <- nbrOfWorkers()
 
-nwork <- getDoParWorkers()
+cat(nnode,"nodes",nwork,"nworkers",nper,"futures/worker","\n")
 
-cat(nnode,'nodes x',ncore,'cores,','chunksize',chunk,'nworkers',nwork,'\n')
+library(tidyverse)
 
 res |>
-  mutate(etime=difftime(t2,t1,units='secs')) |>
-  summarize(stime=as.numeric(sum(etime)),
-    etime=as.numeric(difftime(max(t2),min(t1),units="secs"))) |>
-  mutate(otime=as.numeric(difftime(toc,tic,units='secs')),
+  mutate(etime=difftime(t2,t1,units="secs")) |>
+  summarize(
+    stime=as.numeric(sum(etime)),
+    etime=as.numeric(difftime(max(t2),min(t1),units="secs"))
+  ) |>
+  mutate(
+    otime=as.numeric(difftime(toc,tic,units="secs")),
     ieffic=stime/etime/nwork,
     oeffic=stime/otime/nwork,
     njobs=njobs,
     nnode=nnode,
-    ncore=ncore,
-    nwork=nwork,
-    chunk=chunk) |>
-  melt(id=NULL) |>
-  mutate(y=-seq_along(variable),label=paste0(variable,"\t",signif(value,4))) -> eff
+    nwork=nwork
+  ) |>
+  pivot_longer(everything()) |>
+  mutate(
+    y=-seq_along(name),
+    label=paste0(name,"\t",signif(value,4))
+  ) -> eff
 
-eff |> use_series("label") |> cat(sep="\n")
-eff |> ggplot(aes(x=1,y=y,label=label))+
+eff$label |> cat(sep="\n")
+eff |>
+  ggplot(aes(x=1,y=y,label=label))+
   geom_text(hjust="left")+
   theme_void() -> txt
 
@@ -70,8 +67,8 @@ res |>
   geom_segment()+
   guides(color="none")+
   theme_bw()+
-  labs(x='time',y='job id')+
-  annotate("segment",x=tic,xend=toc,y=0,yend=njobs,color='black') -> pl
+  labs(x="time",y="job id")+
+  annotate("segment",x=tic,xend=toc,y=0,yend=njobs,color="black") -> pl
 
 res |>
   ggplot(aes(x=host,fill=host,color=host))+
@@ -80,17 +77,10 @@ res |>
   guides(fill="none",color="none")+
   theme(axis.text.x=element_text(angle=90,vjust=0.5)) -> pl1
 
+library(grid)
 png(filename="dofuture_test.png",width=7,height=8,
-  units='in',res=300)
+  units="in",res=300)
 print(pl)
 print(txt,vp=viewport(x=0.2,y=0.8,width=0.4,height=0.2))
 print(pl1,vp=viewport(x=0.75,y=0.3,width=0.4,height=0.4))
 dev.off()
-
-res |>
-  subset(select=c(id,x)) |>
-  arrange(id) |>
-  set_rownames(NULL) |>
-  digest()
-
-## 1ad5a2898de4e21b2baf3a7b7c88afe5
